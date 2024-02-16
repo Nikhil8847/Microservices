4ff43aeb51140926e23079e74150fac42a11ddde
Nikhil8847 <dukeforever95@gmail.com>
2024-02-16 12:59:48 +0530

    initial commit

---
+++ Ecom.IdentityServer/Server/Program.cs
@@ -0,0 +1,100 @@
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Server;
+using Server.Data;
+using Server.Services;
+
+var seed = args.Contains("/seed");
+if (seed)
+{
+    args = args.Except(new[] { "/seed" }).ToArray();
+}
+
+var builder = WebApplication.CreateBuilder(args);
+
+var assembly = typeof(Program).Assembly.GetName().Name;
+var defaultConnString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (seed)
+{
+    SeedData.EnsureSeedData(defaultConnString);
+}
+
+
+
+builder.Services.AddDbContext<AspNetIdentityDbContext>(options =>
+    options.UseSqlServer(defaultConnString,
+        b => b.MigrationsAssembly(assembly)));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AspNetIdentityDbContext>();
+
+builder.Services.AddIdentityServer(options =>
+{
+    options.IssuerUri = "http://authenticationserver:80";
+})
+    .AddAspNetIdentity<IdentityUser>()
+    .AddProfileService<CustomProfileService>()
+    .AddConfigurationStore(options =>
+    {
+        options.ConfigureDbContext = b =>
+        b.UseSqlServer(defaultConnString, opt => opt.MigrationsAssembly(assembly));
+    })
+    .AddOperationalStore(options =>
+    {
+        options.ConfigureDbContext = b =>
+        b.UseSqlServer(defaultConnString, opt => opt.MigrationsAssembly(assembly));
+    })
+    .AddDeveloperSigningCredential();
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.Name = "AmCart";
+    //options.Cookie.Domain = "localhost:5446";  //".fmpro.com";
+
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+    options.Cookie.SameSite = SameSiteMode.None;
+
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+    options.SlidingExpiration = true;
+});
+builder.Services.AddControllersWithViews();
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor |
+        ForwardedHeaders.XForwardedProto | ForwardedHeaders.XForwardedHost;
+    // Only loopback proxies are allowed by default.
+    // Clear that restriction because forwarders are enabled by explicit 
+    // configuration.
+    options.KnownNetworks.Clear();
+    options.KnownProxies.Clear();
+});
+
+var app = builder.Build();
+app.UseForwardedHeaders();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseHsts();
+app.UseCookiePolicy(new CookiePolicyOptions
+
+{
+
+    MinimumSameSitePolicy = SameSiteMode.None,
+
+    HttpOnly = HttpOnlyPolicy.Always,
+
+    Secure = CookieSecurePolicy.Always
+
+});
+app.UseIdentityServer();
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
+
+app.Run();
